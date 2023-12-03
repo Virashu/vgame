7f56708960abf31dce401325380be70103ad8f77
Vlad <89295404+Virashu@users.noreply.github.com>
2023-12-03 14:54:42 +0300

    Zammmn 😲

--- src/framework.py
+++ src/framework.py
@@ -1,78 +1,54 @@
 import pygame
 
-import sys
-from abc import abstractmethod
-from typing import Callable, final
+from graphics import Graphics
 
-import graphics
+width, height, framerate = 800, 600, 120
+auto_clear = True
 
-
+pygame.init()
 clock = pygame.time.Clock()
-_lock = [False]
+screen = pygame.display.set_mode((width, height))
+graphics = Graphics(screen)
 
 
-class Game:
-    def __init__(
-        self, width: int = 800, height: int = 600, framerate: int = 60
-    ) -> None:
-        if _lock[0]:
-            raise Exception("Can only create one instance of class")
-        _lock[0] = True
-        self.height = height
-        self.width = width
-        self.framerate = framerate
+def run():
+    running = True
+    # load()
 
-        pygame.init()
-        self.screen = pygame.display.set_mode((width, height))
-        self.graphics = graphics.Graphics(self.screen)
-        self.delta: int = 0
-        self.auto_clear = True
-        self.pressed_keys = ()
+    while running:
+        pressed_keys = tuple(i for i, k in enumerate(pygame.key.get_pressed()) if k)
 
-    @abstractmethod
-    def update(self):
-        # Not implemented: Need multithreading 😕
-        ...
+        delta = clock.get_time()
 
-    @abstractmethod
-    def draw(self):
-        ...
+        if auto_clear:
+            screen.fill((0, 0, 0))
 
-    @abstractmethod
-    def load(self):
-        ...
+        # draw()
 
-    @abstractmethod
-    def exit(self):
-        ...
+        yield delta, pressed_keys
 
-    @final
-    def run(self):
-        self.load()
+        pygame.display.flip()
 
-        while True:
-            self._poll_events()
-            self.pressed_keys = tuple(
-                i for i, k in enumerate(pygame.key.get_pressed()) if k
-            )
-
-            self.delta = clock.get_time()
-
-            if self.auto_clear:
-                self.screen.fill((0, 0, 0))
-
-            self.draw()
-            pygame.display.flip()
-
-            clock.tick(self.framerate)
-
-    def _poll_events(self):
+        clock.tick(framerate)
         for e in pygame.event.get():
             if (
                 e.type == pygame.QUIT
                 or e.type == pygame.KEYDOWN
                 and e.key == pygame.K_q
             ):
-                self.exit()
+                # self.exit()
                 pygame.quit()
-                sys.exit()
+                running = False
+
+
+class Game:
+    def __init__(
+        self, width: int = 800, height: int = 600, framerate: int = 60
+    ) -> None:
+        self.height = height
+        self.width = width
+        self.framerate = framerate
+
+        self.delta: int = 0
+
+        self.pressed_keys = ()
--- src/main.py
+++ src/main.py
@@ -1,40 +1,30 @@
-from typing import final
-from framework import Game
+import framework as game
 from graphics.sprites import Human
 
 
-class MyGame(Game):
-    @final
-    def load(self):
-        self.sx, self.sy = 0, 0
+sx = sy = 0
 
-    @final
-    def draw(self):
-        # Draw grid
-        for i in range(20, self.width, 20):
-            self.graphics.line((i, 0), (i, self.height), (100, 100, 100))
+for r in game.run():
+    delta, pressed_keys = r
 
-        for i in range(20, self.height, 20):
-            self.graphics.line((0, i), (self.width, i), (100, 100, 100))
+    # Draw grid
+    for i in range(20, game.width, 20):
+        game.graphics.line((i, 0), (i, game.height), (100, 100, 100))
 
-        # Draw circle
-        # self.graphics.circle((self.sx * 20 + 10, self.sy * 20 + 10), 10)
-        self.graphics.draw_sprite(Human(self.sx * 20 + 10, self.sy * 20 + 10))
+    for i in range(20, game.height, 20):
+        game.graphics.line((0, i), (game.width, i), (100, 100, 100))
 
-        # Movement
-        if 79 in self.pressed_keys and self.sx < self.width // 20 - 1:
-            self.sx += 1
-        if 80 in self.pressed_keys and self.sx > 0:
-            self.sx -= 1
-        if 81 in self.pressed_keys and self.sy < self.height // 20 - 1:
-            self.sy += 1
-        if 82 in self.pressed_keys and self.sy > 0:
-            self.sy -= 1
+    # Draw circle
+    # game.graphics.circle((game.sx * 20 + 10, game.sy * 20 + 10), 10)
+    game.graphics.draw_sprite(Human(sx * 20 + 10, sy * 20 + 10))
 
-    @final
-    def exit(self):
-        print("Goodbye!")
-
-
-game = MyGame(width=800, height=600, framerate=120)
-game.run()
+    # Movement
+    if 79 in pressed_keys and sx < game.width // 20 - 1:
+        sx += 1
+    if 80 in pressed_keys and sx > 0:
+        sx -= 1
+    if 81 in pressed_keys and sy < game.height // 20 - 1:
+        sy += 1
+    if 82 in pressed_keys and sy > 0:
+        sy -= 1
+print("Goodbye!")
