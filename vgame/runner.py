d1c92ae1a4616bf048b62b1b9c4e7c3551b0e5bc
Vlad <89295404+Virashu@users.noreply.github.com>
2024-02-04 14:16:11 +0300

    Add better keys check

--- vgame/runner.py
+++ vgame/runner.py
@@ -2,6 +2,7 @@
 
 import copy
 import threading
+import pathlib
 
 import pygame
 import pygame.constants as pg_constants
@@ -30,13 +31,12 @@ class Runner:
         self.__class__.clear_lock()
 
     def __init__(self) -> None:
-        self.library = Library()
         self.auto_clear = True
         self._snapshot_update_event = threading.Event()
 
         pygame.init()
 
-        self.game: Scene
+        self.scene: Scene
         self.screen: pygame.Surface
         self._snapshot: Scene
         self._draw_loop_thread: threading.Thread
@@ -48,28 +48,35 @@ class Runner:
         if not self.running:
             return
 
-        self.game: Scene = scene
+        self.scene: Scene = scene
 
-        self.game.graphics.library = self.library
-        self.game.load()
+        self.scene.graphics.library = Library()
+        self.scene.load()
 
-        pygame.display.set_caption(self.game.title)
+        if not pathlib.Path(self.scene.graphics.library.path).exists():
+            raise RuntimeError(
+                "Sprite library path does not exist or not set: "
+                f"{self.scene.graphics.library.path}"
+                '\nPlease use\n\tself.graphics.library.path = "<path>"'
+            )
+
+        pygame.display.set_caption(self.scene.title)
 
         self.screen: pygame.Surface = pygame.display.set_mode(
-            (self.game.width, self.game.height)
+            (self.scene.width, self.scene.height)
         )
 
         # Snapshot is a completed (fully updated) copy of the game
-        self._snapshot: Scene = self.game
+        self._snapshot: Scene = self.scene
 
-        self.game.graphics.surface = self.screen
+        self.scene.graphics.surface = self.screen
 
         self._run()
 
     def _draw_loop(self):
         clock = pygame.time.Clock()
 
-        while self.game.running:
+        while self.scene.running:
             self._snapshot_update_event.wait()
             snapshot = self._snapshot
             self._snapshot_update_event.clear()
@@ -77,32 +84,35 @@ class Runner:
             if self.auto_clear:
                 self.screen.fill((0, 0, 0))
 
-            self.game.graphics_delta = snapshot.graphics_delta = (
+            self.scene.graphics_delta = snapshot.graphics_delta = (
                 clock.get_time() / 1000
             )  # ms -> s
-            self.game.fps = snapshot.fps = clock.get_fps()
+            self.scene.fps = snapshot.fps = clock.get_fps()
 
             snapshot.draw()
 
             pygame.display.flip()
 
-            clock.tick(self.game.framerate)
+            clock.tick(self.scene.framerate)
 
     def _update_loop(self) -> None:
         update_clock = pygame.time.Clock()
 
-        while self.game.running:
-            self.game.delta = update_clock.get_time() / 1000  # ms -> s
-            self.game.tps = update_clock.get_fps()
+        while self.scene.running:
+            self.scene.delta = update_clock.get_time() / 1000  # ms -> s
+            self.scene.tps = update_clock.get_fps()
 
-            self.game.update()
+            self.scene.update()
 
             if not self._snapshot_update_event.is_set():
-                self._snapshot: Scene = copy.deepcopy(self.game)
+
+                self._snapshot: Scene = copy.deepcopy(self.scene)
+
                 # self._snapshot.graphics.set_surface(self.screen)
                 self._snapshot_update_event.set()
 
-            update_clock.tick(self.game.tickrate)
+            update_clock.tick(self.scene.tickrate)
+
             self._poll_events()
 
     def _run(self) -> None:
@@ -118,13 +128,16 @@ class Runner:
             if e.type == pg_constants.QUIT:
                 self._stop()
             elif e.type == pg_constants.KEYDOWN:
-                self.game.pressed_keys.add(e.key)
+                self.scene.pressed_keys.add(e.key)
+                self.scene.clicked_keys.add(e.key)
+
             elif e.type == pg_constants.KEYUP:
-                self.game.pressed_keys.discard(e.key)
+                self.scene.pressed_keys.discard(e.key)
+                self.scene.released_keys.add(e.key)
 
     def _stop(self) -> None:
         self._snapshot_update_event.set()
-        self.game.stop()
-        self.game.exit()
+        self.scene.stop()
+        self.scene.exit()
         self.running = False
         pygame.quit()
